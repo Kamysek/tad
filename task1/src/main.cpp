387e3ee43435f10407ce4eeb608695fada2cf932
Josef Kamysek <josef@kamysek.com>
2020-11-20 17:55:33 +0100

    - Started with task1b

--- task1/src/main.cpp
+++ task1/src/main.cpp
@@ -29,14 +29,15 @@ The values printed in the terminal from task1 are used to plot the camera positi
 This is done using Matlab (see matlab/plot1a.m and matlab/plot1a.fig)
 
 */
-void task1a()
+map<ImageID, pair<Mat, Mat>> task1a()
 {
 
     // Load 3D teabox coordinates, image location and provided 2D image coordinates
     map<TeaBoxCorner, Point3d> tb3DCoordinates = input::teabox3DCoordinates();
     map<ImageID, string> imgLocation = input::imageLocation();
     map<ImageID, map<TeaBoxCorner, Point2d>> img2DCoordinates = input::image2DCoordinates();
-    
+    map<ImageID, pair<Mat, Mat>> RotationMatrixAndTranslationVector;
+
     // Cycle through images
     for ( int iidInt = DSC_9743; iidInt != LASTIID; iidInt++ )
     {
@@ -48,15 +49,15 @@ void task1a()
         if(img2DCoordinates.empty())
         {
             cout << "Could not load 2D coordinates of image: " + std::to_string(iid) << endl;
-            return;
+            throw "Could not load 2D coordinates of image: " + std::to_string(iid);
         }
 
         // Load image
         Mat img = imread(imgLocation[iid], IMREAD_COLOR);
         if(img.empty())
         {
-            cout << "Could not load image " + std::to_string(iid) + "from path: " + std::filesystem::current_path().generic_string() << endl;
-            return;
+            cout << "Could not load image " + std::to_string(iid) + " from path: " + std::filesystem::current_path().generic_string() << endl;
+            throw "Could not load image " + std::to_string(iid) + " from path: " + std::filesystem::current_path().generic_string();
         }
 
         // Create two vector one containing the 3D teabox coordinates and the second the corresponding 2D image coordinates
@@ -102,10 +103,83 @@ void task1a()
         cout << vector.size << endl;
         rotationMatrix = rotationMatrix.t() * vector;
         global::printMat("Camera Direction", rotationMatrix);
+
+        RotationMatrixAndTranslationVector.insert(pair(iid, pair(rotationMatrix, translationVect)));
+    }
+    return RotationMatrixAndTranslationVector;
+}
+
+/*
+
+Solution for task1
+
+NOTE: 
+Images are only loaded correct if the program is exectuted from /build/src! 
+The values printed in the terminal from task1 are used to plot the camera positions. 
+This is done using Matlab (see matlab/plot1a.m and matlab/plot1a.fig)
+
+*/
+
+void task1b(){
+
+    // Load 3D teabox coordinates, image location and provided 2D image coordinates
+    map<TeaBoxCorner, Point3d> tb3DCoordinates = input::teabox3DCoordinates();
+    map<ImageID, string> imgLocation = input::imageLocation();
+    map<ImageID, map<TeaBoxCorner, Point2d>> img2DCoordinates = input::image2DCoordinates();
+    
+    // Cycle through images
+    for ( int iidInt = DSC_9743; iidInt != LASTIID; iidInt++ )
+    {
+        ImageID iid = static_cast<ImageID>(iidInt);
+        cout << "Calculated Information for Image" + std::to_string(iid) << endl;
+
+        // Load 2D coordinates for given image
+        map<TeaBoxCorner, Point2d> img2DCords = img2DCoordinates[iid];
+        if(img2DCoordinates.empty())
+        {
+            cout << "Could not load 2D coordinates of image: " + std::to_string(iid) << endl;
+            return;
+        }
+
+        // Map 2D points to 3D rays
+
+
+
+
+        // Load image
+        Mat img = imread(imgLocation[iid], IMREAD_GRAYSCALE);
+        if(img.empty())
+        {
+            cout << "Could not load image " + std::to_string(iid) + "from path: " + std::filesystem::current_path().generic_string() << endl;
+            return;
+        }
+
+        // Detect the keypoints using SIFT Detector
+        Ptr<SIFT> detector = SIFT::create();
+        std::vector<KeyPoint> keypoints;
+        detector->detect(img, keypoints);
+        
+        // Draw keypoints on picture
+        Mat output;
+        drawKeypoints(img, keypoints, output);
+        
+        // Show detected keypoints
+        imshow("Keypoints 1", output);
+       
+        waitKey(0);
+
     }
 }
 
-int main() {
+// Point3d convert2DPointTo3DRay(Point2d m){
+
+//     // Calculate 
+
+//     retunr 
+// }
+
+int main()
+{
 
     cout << "Solution for task1 \n \n" 
             "NOTE: \n"
@@ -114,6 +188,8 @@ int main() {
             "This is done using Matlab (see matlab/plot1a.m and matlab/plot1a.fig) \n"
     << endl;
 
-    task1a();
+    map<ImageID, pair<Mat, Mat>> RotationAndTranslation = task1a();
+    
+    //task1b();
 
 }
